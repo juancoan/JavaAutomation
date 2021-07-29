002836426269d5c6d547ee5303e3d76e8b084f23
Juan Cordoba <wivercr@gmail.com>
2021-07-29 17:46:08 -0600

    Subiendo archivos de Tarea 2 y poniendo comentarios.

--- src/main/java/Tarea_2.java
+++ src/main/java/Tarea_2.java
@@ -11,24 +11,27 @@ public class Tarea_2 {
         String pathToDriver = Main.class.getResource("/chromedriver.exe").getPath(); //Windows
         System.setProperty("webdriver.chrome.driver", pathToDriver);
 
+        //creando instancia y abriendo pagina
         WebDriver driver = new ChromeDriver();
-
         driver.get("https://demo.opencart.com/");
 
+        //buscando el campo de busqueda
         WebElement searchElement = driver.findElement(By.name("search"));
         searchElement.sendKeys("macbook");
 
+        //click en el boton para ejecutar busqueda
         WebElement searchButton = driver.findElement(By.xpath("(//span[1]/button)[1]"));
         searchButton.click();
 
-        //String myXpath = "(//span[text()='Add to Cart'])[1]";
-
+        //buscando el primer link de la macbook y click
         WebElement macLink1 = driver.findElement(By.xpath("(//div[contains(@class, 'caption')]/h4/a)[1]"));
         macLink1.click();
 
+        //buscando el boton de add to cart
         WebElement addToCart = driver.findElement(By.id("button-cart"));
         addToCart.click();
 
+        //luego de darle click en add to car, hago assert de que me sale la palabra "Qty" verificar que si le di click
         WebElement message = driver.findElement(By.xpath("//label[contains(text(), \"Qty\")]"));
         Assert.assertTrue(message.isDisplayed());
 
